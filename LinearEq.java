cebe6c990aeee2329171abd0703f2226c136d5b0
danielzecharia <daniel.zecharia01@post.runi.ac.il>
2023-12-21 15:11:28 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -11,6 +11,6 @@ public static void main(String[] args) {
   double b= Double.parseDouble(args[1]);
   double c= Double.parseDouble(args[2]);
   System.out.println( a + " * x + " + b + " = " + c);
-  System.out.println(" x = " + ((c-b)/a));
+  System.out.println("x = " + ((c-b)/a));
 }
 }
