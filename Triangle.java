780b9377c92815adbcba1e257bccdc0fc503fc16
danielzecharia <daniel.zecharia01@post.runi.ac.il>
2023-12-21 15:21:17 +0200

    update

--- Triangle.java
+++ Triangle.java
@@ -10,15 +10,9 @@ public class Triangle {
   int b= Integer.parseInt(args[1]);
   int c= Integer.parseInt(args[2]);
 
-  if(a+b>c)
-    if(a+c>b)
-      if(b+c>a)
-        System.out.println( a + " , " + b + " , " + c + " : true ");
-      else
-        System.out.println( a + " , " + b + " , " + c + " : false ");
-     else 
-      System.out.println( a + " , " + b + " , " + c + " : false ");
+  if((a+b>c)&&(a+c>b)&&(b+c>a))
+    System.out.println( a + " , " + b + " , " + c + " : true");
   else
-    System.out.println( a + " , " + b + " , " + c + " : false ");
+    System.out.println( a + " , " + b + " , " + c + " : false");
 	}
 }
