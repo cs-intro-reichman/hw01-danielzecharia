06324eb13a628ac5a618bc2926cad2f1d5ab0dfb
danielzecharia <daniel.zecharia01@post.runi.ac.il>
2023-12-21 15:00:58 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -4,7 +4,8 @@
  * computes x, and prints the result.
  * Treats the three arguments as well as the computed value as double values
  */
-public class LinearEq {
+public class LinearEq 
+public static void main(String[] args) {
 
   double a= Double.parseDouble(args[0]);
   double b= Double.parseDouble(args[1]);
