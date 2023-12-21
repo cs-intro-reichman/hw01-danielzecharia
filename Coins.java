0f5264ca8e09c80a3eb470b52c5059eebd1f6702
danielzecharia <daniel.zecharia01@post.runi.ac.il>
2023-12-21 14:54:03 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -5,7 +5,7 @@
 public class Coins {
 public static void main(String[] args) {
  int a= Integer.parseInt(args[0]);
- System.out.println("use " + (a/25) +" quarters and " + (a%25) + " cents");
+ System.out.println("Use "+(a/25)+" quarters and "+(a%25)+" cents");
 	
 	}
 }
