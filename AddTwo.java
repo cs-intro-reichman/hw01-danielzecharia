49d8bbc75f474e6bcdefd394de3d919c630a4860
danielzecharia <daniel.zecharia01@post.runi.ac.il>
2023-12-21 14:45:16 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -6,8 +6,6 @@ public class AddTwo {
 		int a = Integer.parseInt(args[0]);
 
 	int b = Integer.parseInt(args[1]);
-
-	System.out.println("java AddTwo "+ a + " " + b);
 	
 	System.out.println( a + " + " + b + " = " + (a+b));
 	}
